fd5abe74b3d61cb5ae7167cb5dcabad01f6220d7
wtfurog_ <84697027+wendellfurog@users.noreply.github.com>
2022-10-07 10:37:47 +0800

    Added create new transaction feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 function App() {
@@ -7,7 +7,20 @@ function App() {
     amount: 0,
     description: '',
     date: ''
-  })
+  });
+  const [transactions, setTransactions] = useState([]);
+
+
+  useEffect(() => {
+    fetchTransactions();
+  }, [])
+
+  async function fetchTransactions() {
+    // Default method is GET
+    const res = await fetch('http://localhost:4000/transaction');
+    const { data } = await res.json();
+    setTransactions(data);
+  }
 
   function handleInput(e) {
     setForm({
@@ -18,9 +31,15 @@ function App() {
     e.preventDefault();
     const res = await fetch('http://localhost:4000/transaction', {
       method: 'POST',
-      body: form
+      body: JSON.stringify(form),
+      headers: {
+        "content-type": "application/json"
+      }
     })
-    console.log(res);
+    if (res.ok) {
+      // Page adds new transaction in realtime if response is successfull
+      fetchTransactions();
+    }
   }
 
 
@@ -49,6 +68,28 @@ function App() {
         />
         <button type="submit">Submit</button>
       </form>
+      <br/>
+      <section>
+        <table>
+          <thead>
+            <tr>
+            <th>Amount</th>
+            <th>Description</th>
+            <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction) => (
+            <tr key={transaction._id}>
+              <td>{transaction.amount}</td>
+              <td>{transaction.description}</td>
+              <td>{transaction.data}</td>
+            </tr>
+            ))}
+          </tbody>
+        </table>
+
+      </section>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import bodyParser from 'body-parser'
+import Transaction from './models/Transaction.js';
+
 
 const PORT = 4000;
 const app = express();
 
-app.use(cors);
+app.use(cors());
+app.use(bodyParser.json());
 
 
 await mongoose
@@ -15,9 +19,29 @@ await mongoose
   .then(() => console.log('MongoDB connection is successfull'))
   .catch((err) => console.log(err));
 
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
+
+
+app.get('/transaction', async (req, res) => {
+  const transaction = await Transaction.find({}).sort({ createdAt: -1 });
+  res.json({ data: transaction });
+});
+
+
+app.post('/transaction', async (req, res) => {
+  const {amount, description, date} = req.body;
+  const transaction = new Transaction ({
+      amount,
+      description,
+      date,
+  });
+  await transaction.save();
+  res.json({ message: 'Success' });
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
